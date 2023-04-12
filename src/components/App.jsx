cda065f8997617c176b45ee1c1cff60a1fd165ac
Sergii79 <tsymbal.sergiy@gmail.com>
2023-04-12 15:22:59 +0300

    commit second

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
 import { RecipeList } from "./RecipeList";
-import recipes from "../recipes.json";
+
 
 
 export const App = () => {
   return (
     <div>
-      <RecipeList items={recipes}/>
+      <RecipeList items={[1, 2, 3]}/>
     </div>
   );
 };
