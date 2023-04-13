edd5a27934d7540ee49be5433df65747b11b9e12
Sergii79 <tsymbal.sergiy@gmail.com>
2023-04-13 18:47:22 +0300

    commit second

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,22 +1,24 @@
-export const RecipeCard = ({ item }) => {
+import PropTypes from 'prop-types'
+
+export const RecipeCard = ({ item: { image, name, time, servings, calories, difficulty } }) => {
     return (
         <div>
-            <img src={item.image} alt={item.name} width="240" />
-            <h2>{item.name}</h2>
+            <img src={image} alt={name} width="240" />
+            <h2>{name}</h2>
 
             <div style={{
                 display: "flex", gap: 10}}>
                 <div>
                     <span>icon</span>
-                    <p>{item.time}min</p>
+                    <p>{time}min</p>
                 </div>
                 <div>
                     <span>icon</span>
-                    <p>{item.servings}servings</p>
+                    <p>{servings}servings</p>
                 </div>
                 <div>
                     <span>icon</span>
-                    <p>{item.calories}calories</p>
+                    <p>{calories}calories</p>
                 </div>
             </div>
 
@@ -29,4 +31,15 @@ export const RecipeCard = ({ item }) => {
                 </div>
             </div>
         </div>);
- };
+};
+ 
+RecipeCard.propTypes = {
+    item: PropTypes.shape({
+        image: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        time: PropTypes.number.isRequired,
+        servings: PropTypes.number.isRequired,
+        calories: PropTypes.number.isRequired,
+        difficulty: PropTypes.oneOf(["easy", "medium", "hard"]).isRequired,
+    }).isRequired,
+};
