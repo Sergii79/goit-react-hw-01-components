b233ef434f0da88d4ab926d144c9a6732b0d70d5
Sergii79 <tsymbal.sergiy@gmail.com>
2023-04-13 09:45:45 +0300

    commit second

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,8 +1,12 @@
+import { RecipeCard } from "./RecipeCard";
+
 export const RecipeList = ({ items }) => {
     return (
         <ul>
             {items.map(item => (
-                <li key={item.id}>{item.name}</li>
+                <li key={item.id}>
+                    <RecipeCard item={item} />
+                </li>
             ))}
         </ul>
     );
