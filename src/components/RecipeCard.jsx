2669fdb1ba2fa182ac1bbf989d544f0f043c12a9
Sergii79 <tsymbal.sergiy@gmail.com>
2023-04-13 15:06:54 +0300

    commit second

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -3,5 +3,30 @@ export const RecipeCard = ({ item }) => {
         <div>
             <img src={item.image} alt={item.name} width="240" />
             <h2>{item.name}</h2>
+
+            <div style={{
+                display: "flex", gap: 10}}>
+                <div>
+                    <span>icon</span>
+                    <p>{item.time}min</p>
+                </div>
+                <div>
+                    <span>icon</span>
+                    <p>{item.servings}servings</p>
+                </div>
+                <div>
+                    <span>icon</span>
+                    <p>{item.calories}calories</p>
+                </div>
+            </div>
+
+            <div>
+                <h3>Difficulty</h3>
+                <div>
+                    <span>Easy</span>
+                    <span>Medium</span>
+                    <span>Hard</span>
+                </div>
+            </div>
         </div>);
  };
