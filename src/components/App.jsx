ea35bd7b1934e66f2e232505de96efc5a78e5f21
Sergii79 <tsymbal.sergiy@gmail.com>
2023-04-14 21:41:18 +0300

    commit second

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,15 @@
 import { RecipeList } from "./RecipeList/RecipeList";
 import recipes from "../recipes.json";
 import { GlobalStyle } from "./GlobalStyle";
-// import { Layout } from "./Layout/Layout";
+import { Layout } from "./Layout/Layout";
 
 
 export const App = () => {
   return (
-    <>
+    <Layout>
       <RecipeList items={recipes} />
       <GlobalStyle />
-    </>
+    </Layout>
   );
 };
 
