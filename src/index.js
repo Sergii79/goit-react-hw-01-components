fc0629e3ca80fe0b1c15e9f0ba7fa7f5dcf3d95c
Sergii79 <tsymbal.sergiy@gmail.com>
2023-04-19 15:40:20 +0300

    commit seventh

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { ThemeProvider } from 'styled-components';
-import './index.css';
 
 const theme = {
   colors: {
     text: 'blue',
     accent: 'tomato',
-  },
+    background: 'yellow',
+  },  
 };
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -21,18 +21,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
 console.log("Hello!");
 
-// const imageUrl =
-//   "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640";
-// const productPrice = 10.99;
-
-// const product = (
-//   <div>
-//     <img src={imageUrl} alt="Tacos With Lime" width="640" />
-//     <h2>Tacos With Lime</h2>
-//     <p>Price: {productPrice}$</p>
-//     <button type="button">Add to cart</button>
-//   </div>
-// );
 
 
 
